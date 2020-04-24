7e7e497c571ae91d5d1556ecb056a19c000d2612
Konstantin <konstantin.faleev@gmail.com>
2020-04-24 17:08:46 +0300

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/edit'
-  get 'lists/create'
-  get 'lists/update'
-  get 'lists/destroy'
-  get 'tasks/new'
-  get 'tasks/create'
-  get 'tasks/update'
-  get 'tasks/done'
-  get 'tasks/task_up'
-  get 'tasks/task_down'
-  get 'tasks/destroy'
-  devise_for :users
   root 'pages#home'
-  get 'pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :users do
+    resources :lists do
+      resources :tasks do
+        member do
+          put :task_up
+          put :task_down
+          patch :done
+          patch :deadline
+          get :deadline
+        end
+      end
+    end
+  end
+
+  resources :lists do
+    resources :tasks do
+    end
+  end
 end
