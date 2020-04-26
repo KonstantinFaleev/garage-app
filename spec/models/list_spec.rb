d00c5ba132df8e5439a7f40c2f0a68d61b5feb90
Konstantin <konstantin.faleev@gmail.com>
2020-04-26 20:02:04 +0300

    added test rspec for models

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "cannot be empty" do
+    todo_list = List.new
+    expect (todo_list).not_to be_valid
+  end
+
+  it "title not longer than 255 symbols" do
+    todo_list = List.new(title: 'a' * 254)
+    expect (todo_list).to be_valid
+  end
+
+  it 'list validates presence' do
+    record = List.new
+    record.title = ''
+    record.valid?
+    expect(record.errors[:title]).to include("can't be blank")
+
+    record.title = 'test'
+    expect(record.errors[:title]).to_not include("can't be blank")
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "cannot be empty" do
+    todo_list = Task.new
+    expect (todo_list).not_to be_valid
+  end
+
+  it "name not longer than 255 symbols" do
+    todo_list = Task.new(name: 'a' * 254)
+    expect (todo_list).to be_valid
+  end
+
+  it 'task validates presence' do
+    record = Task.new
+    record.name = ''
+    record.valid?
+    expect(record.errors[:name]).to include("can't be blank")
+
+    record.name = 'test'
+    expect(record.errors[:name]).to_not include("can't be blank")
+  end
 end
